965088d6eaefed0b320959af6e1d6b9ef48f7a5f
Mark Kerzner <mark@Marks-MacBook-Pro.local>
2023-05-24 11:59:08 -0500

    dev py

--- 06-workshop/Langchain_Semantic_Search_Pinecone.py
+++ 06-workshop/Langchain_Semantic_Search_Pinecone.py
@@ -1,2 +1,8 @@
-import openai
 import os
+import openai
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+openai.api_key  = os.getenv('OPENAI_API_KEY')
+MODEL="gpt-4"
+
