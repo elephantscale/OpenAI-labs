5af27c3dde604753a4e78373af2d047e99b1aea8
Mark Kerzner <mark@elephantscale.com>
2023-05-25 19:19:33 -0500

    MosesAI works

--- 06-workshop/main.py
+++ 06-workshop/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
+from Langchain_Semantic_Search_Pinecone import get_answer
 
 app = FastAPI(
     title="MosesAI API",
@@ -13,11 +14,13 @@ class Item(BaseModel):
     question: str
     answer: str
 
+
+@app.get("/items2/{question}", response_model=Item, summary="Read item", description="Read a specific item by its id.")
+def read_item(question: str):
+    answer = get_answer(question)
+    return {"question": question, "answer": answer}
+
+
 @app.get("/", summary="Read the root", description="Shalom. I am MosesAI, and right now, I know Talmud")
 def read_root():
     return {"Hello": "Shalom"}
-
-@app.get("/items2/{question}", response_model=Item, summary="Read item", description="Read a specific item by its id.")
-def read_item(question: str, q: str = None):
-    answer = "Your question was: " + question + ", and the answer is bla-bla-bla"
-    return {"question": question, "answer": answer}
