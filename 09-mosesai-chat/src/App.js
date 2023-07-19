d105324fd65c0641069f5fbf74859577d63a25ea
Ubuntu <ubuntu@ip-172-31-30-69.us-east-2.compute.internal>
2023-07-19 04:09:40 +0000

    Change API url

--- 09-mosesai-chat/src/App.js
+++ 09-mosesai-chat/src/App.js
@@ -34,7 +34,7 @@ const Chat = () => {
 
         try {
             // const response = await axios.post('http://localhost:8000/ask', {
-            const response = await axios.post('http://mosesai.org:8000/ask', {
+            const response = await axios.post('/api/ask', {
                 question: newMessage
             });
             const botMessage = { user: 'MosesAI', text: response.data.answer };
@@ -73,7 +73,7 @@ const Chat = () => {
             <div className="question-div">
                 <input className="question-input" placeholder="Send a question" value={newMessage} onKeyDown={handleEnterKeyMessage} onChange={handleNewMessageChange} />
                 <button className="question-button" onClick={handleSendMessage}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-send" viewBox="0 0 16 16">
                         <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"/>
                     </svg>
                 </button>
