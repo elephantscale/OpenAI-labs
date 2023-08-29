88507006990afb53aba986fd59e1cf3699ade29c
Mark Kerzner <mark@elephantscale.com>
2023-08-29 15:46:28 -0500

    moved MosesAI to its own repository

--- 06-workshop/deploy_fastapi.sh
+++
@@ -1,2 +0,0 @@
-# Start a fastapi application
-sudo /home/ubuntu/anaconda3/bin/uvicorn  main:app --host 0.0.0.0 --port 80
--- 06-workshop/run_fastapi.sh
+++
@@ -1,2 +0,0 @@
-# Start a fastapi application for development
-uvicorn main:app --host 0.0.0.0 --port 8000
