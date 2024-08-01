09e9218de28476349143a621b1a58f1d54bb4a20
Neha <nehadubey0605@gmail.com>
2024-08-01 16:56:03 +0400

    modified utils

--- 38-AI-Agentic-Design-Patterns-with-AutoGen/utils.py
+++ 38-AI-Agentic-Design-Patterns-with-AutoGen/utils.py
@@ -2,7 +2,6 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 
-# these expect to find a .env file at the directory above the lesson.                                                                                                                     # the format for that file is (without the comment)                                                                                                                                       #API_KEYNAME=AStringThatIsTheLongAPIKeyFromSomeService                                                                                                                                     
 def load_env():
     _ = load_dotenv(find_dotenv())
 
