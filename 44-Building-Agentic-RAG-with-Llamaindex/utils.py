3b9373c5f2c41fba0dac75478b7fef5675a122bb
Neha <nehadubey0605@gmail.com>
2024-08-09 14:05:41 +0400

    added lab 44

---
+++ 44-Building-Agentic-RAG-with-Llamaindex/utils.py
@@ -0,0 +1,129 @@
+from typing import List, Optional
+
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, SummaryIndex
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.core.query_engine.router_query_engine import RouterQueryEngine
+from llama_index.core.selectors import LLMSingleSelector
+from llama_index.core.tools import FunctionTool, QueryEngineTool
+from llama_index.core.vector_stores import MetadataFilters, FilterCondition
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.llms.openai import OpenAI
+from llama_index.llms.openai import OpenAI as AbstractOpenAI
+from llama_index.core import Settings
+
+
+class OpenAI(AbstractOpenAI):
+    def _prepare_chat_with_tools(self):
+        pass
+
+
+def get_router_query_engine(file_path: str, llm=None, embed_model=None):
+    """Get router query engine."""
+    llm = llm or OpenAI(model="gpt-3.5-turbo")
+    embed_model = embed_model or OpenAIEmbedding(model="text-embedding-ada-002")
+
+    # load documents
+    documents = SimpleDirectoryReader(input_files=[file_path]).load_data()
+
+    splitter = SentenceSplitter(chunk_size=1024)
+    nodes = splitter.get_nodes_from_documents(documents)
+
+    summary_index = SummaryIndex(nodes)
+    vector_index = VectorStoreIndex(nodes, embed_model=embed_model)
+
+    summary_query_engine = summary_index.as_query_engine(
+        response_mode="tree_summarize",
+        use_async=True,
+        llm=llm
+    )
+    vector_query_engine = vector_index.as_query_engine(llm=llm)
+
+    summary_tool = QueryEngineTool.from_defaults(
+        query_engine=summary_query_engine,
+        description=(
+            "Useful for summarization questions related to MetaGPT"
+        ),
+    )
+
+    vector_tool = QueryEngineTool.from_defaults(
+        query_engine=vector_query_engine,
+        description=(
+            "Useful for retrieving specific context from the MetaGPT paper."
+        ),
+    )
+
+    query_engine = RouterQueryEngine(
+        selector=LLMSingleSelector.from_defaults(),
+        query_engine_tools=[
+            summary_tool,
+            vector_tool,
+        ],
+        verbose=True
+    )
+    return query_engine
+
+
+def get_doc_tools(
+        file_path: str,
+        name: str,
+) -> str:
+    """Get vector query and summary query tools from a document."""
+
+    # load documents
+    documents = SimpleDirectoryReader(input_files=[file_path]).load_data()
+    splitter = SentenceSplitter(chunk_size=1024)
+    nodes = splitter.get_nodes_from_documents(documents)
+    vector_index = VectorStoreIndex(nodes)
+
+    def vector_query(
+            query: str,
+            page_numbers: Optional[List[str]] = None
+    ) -> str:
+        """Use to answer questions over the MetaGPT paper.
+
+        Useful if you have specific questions over the MetaGPT paper.
+        Always leave page_numbers as None UNLESS there is a specific page you want to search for.
+
+        Args:
+            query (str): the string query to be embedded.
+            page_numbers (Optional[List[str]]): Filter by set of pages. Leave as NONE
+                if we want to perform a vector search
+                over all pages. Otherwise, filter by the set of specified pages.
+
+        """
+
+        page_numbers = page_numbers or []
+        metadata_dicts = [
+            {"key": "page_label", "value": p} for p in page_numbers
+        ]
+
+        query_engine = vector_index.as_query_engine(
+            similarity_top_k=2,
+            filters=MetadataFilters.from_dicts(
+                metadata_dicts,
+                condition=FilterCondition.OR
+            )
+        )
+        response = query_engine.query(query)
+        return response
+
+    vector_query_tool = FunctionTool.from_defaults(
+        name=f"vector_tool_{name}",
+        fn=vector_query
+    )
+
+    summary_index = SummaryIndex(nodes)
+    summary_query_engine = summary_index.as_query_engine(
+        response_mode="tree_summarize",
+        use_async=True,
+    )
+    summary_tool = QueryEngineTool.from_defaults(
+        name=f"summary_tool_{name}",
+        query_engine=summary_query_engine,
+        description=(
+            "Use ONLY IF you want to get a holistic summary of MetaGPT. "
+            "Do NOT use if you have specific questions over MetaGPT."
+        ),
+    )
+
+    return vector_query_tool, summary_tool
