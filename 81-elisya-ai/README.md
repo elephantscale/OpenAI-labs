91573b8850f867aff835826baca29fdba3a5b386
Mark Kerzner <mark@elephantscale.com>
2025-08-19 17:57:35 -0500

    wear

---
+++ 81-elisya-ai/README.md
@@ -0,0 +1,10 @@
+# To start
+
+python3.12 -m venv .venv
+source .venv/bin/activate
+pip install elysia-ai
+elysia start
+
+
+pip install elysia-ai
+elysia start
---
+++ 81-elisya-ai/src/start.py
@@ -0,0 +1,15 @@
+import os
+import weaviate
+from weaviate.classes.init import Auth
+
+# Best practice: store your credentials in environment variables
+weaviate_url = os.environ["WEAVIATE_URL"]
+weaviate_api_key = os.environ["WEAVIATE_API_KEY"]
+
+# Connect to Weaviate Cloud
+client = weaviate.connect_to_weaviate_cloud(
+    cluster_url=weaviate_url,
+    auth_credentials=Auth.api_key(weaviate_api_key),
+)
+
+print(client.is_ready())
